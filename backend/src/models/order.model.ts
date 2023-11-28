ab25f8e75e52f37cc8c6be8f0c1483f340ae6418
Nivitha <nivithask@gmail.com>
2023-11-28 22:28:49 +0530

    Adding Order Model

---
+++ backend/src/models/order.model.ts
@@ -0,0 +1,40 @@
+import { Schema } from "mongoose";
+import { Food, FoodSchema } from "./food.model";
+
+export interface LatLng {
+    lat: string;
+    lng: string;
+}
+
+export const LatLngSchema = new Schema<LatLng>(
+    {
+        lat: {type: String, required: true},
+        lng: {type: String, required: true}
+    }
+)
+
+export interface OrderItem {
+    food: Food;
+    price: number;
+    quantity: number;
+}
+
+export const OrderItemSchema = new Schema<OrderItem>(
+    {
+        food: {type: FoodSchema, required: true},
+        price: {type: Number, required: true},
+        quantity: {type: Number, required: true}
+    }
+)
+
+export class Order {
+    id!: number;
+    items!: OrderItem[];
+    totalPrice!: number;
+    name!: string;
+    address!: string;
+    addressLatLng!: LatLng;
+    paymentId!: string;
+    createdAt!: string;
+    status!: string;
+}
